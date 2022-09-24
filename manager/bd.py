b3cd699e9d63480b636beb93186d7716a7f73354
dtisoy <danieltisoy2001@gmail.com>
2022-09-23 19:15:53 -0500

    create db class

---
+++ manager/bd.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+class Db:
+    """
+    - db queries, add, delete, get, update for a basic database
+    - create db tables
+    """
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def add(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def get_by_id(self):
+        pass
+
+    def get_all(self):
+        pass
