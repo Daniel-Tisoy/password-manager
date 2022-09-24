80948cad10c327cf757495a51339b8598e52c1b5
dtisoy <danieltisoy2001@gmail.com>
2022-09-23 19:17:27 -0500

    add db connection with sqlite3 to run queries

--- manager/bd.py
+++ manager/bd.py
@@ -10,6 +10,22 @@ class Db:
     def __init__(self, db_name):
         self.db_name = db_name
 
+    def run_query(self, query, data=()):
+        """Run SQL queries with sqlite3 library
+
+        Args:
+            query (str): SQL query string
+            data (tuple, optional): if the sql need values, add them here. Defaults to ().
+
+        Returns:
+            Object: Object sqlite3 cursor
+        """
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            result = cursor.execute(query, data)
+            connection.commit()
+
+        return result
     def add(self):
         pass
 
